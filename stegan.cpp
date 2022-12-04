f96da5ef51a8f2889b869d42df14a95ca1c9542d
Josh Hunt <josh@DESKTOP-F1P25IH>
2022-12-04 11:31:01 -0500

    Maybe working?

--- stegan.cpp
+++ stegan.cpp
@@ -6,11 +6,18 @@
 #include <cstddef>
 #include <typeinfo>
 
+#define HEADER_SIZE sizeof(int) * 8
+
 OIIO_NAMESPACE_USING
 using namespace std;
 
 int img_width = 500, img_height = 500, channels = 4;
+int hidden_width;
+int hidden_height;
+int hidden_channels;
+
 unsigned char* pixmap;
+unsigned char* hidden_pixmap;
 
 ImageBuf readBuffer;
 ImageBuf hiddenBuffer;
@@ -84,6 +91,7 @@ void readImage(string inputName, string hiddenName)
         img_width = spec.width;
         img_height = spec.height;
         channels = spec.nchannels;
+        cout << channels << endl;
 
         pixmap = new unsigned char[4 * img_width * img_height * channels]();
 
@@ -98,53 +106,54 @@ void readImage(string inputName, string hiddenName)
         // img_width *= 2;
         // img_height *= 2;
 
-        const ImageSpec &h_spec = inp->spec();
-        int hidden_width = h_spec.width;
-        int hidden_height = h_spec.height;
-        int hidden_channels = h_spec.nchannels;
+        const ImageSpec &h_spec = inp2->spec();
+        hidden_width = h_spec.width;
+        hidden_height = h_spec.height;
+        hidden_channels = h_spec.nchannels;
+        cout << hidden_channels << endl;
 
         hiddenBuffer = ImageBuf(hiddenName);
         hiddenBuffer.read (0, 0, true, TypeDesc::UINT8);
-        hiddenBuffer = ImageBufAlgo::flip(readBuffer);
+        hiddenBuffer = ImageBufAlgo::flip(hiddenBuffer);
 
         // Resize hidden image to make smaller to more easily fit in other image
-        int channelsToRead = 3;
+        // int channelsToRead = 3;
 
-        ROI newROI (0, hidden_width/2, 0, hidden_height/2, 0, 1, channelsToRead);
+        ROI newROI (0, hidden_width/4, 0, hidden_height/4, 0, 1, hidden_channels);
         hiddenBuffer = ImageBufAlgo::resize(hiddenBuffer, "", 0, newROI);
-        hidden_width /= 2;
-        hidden_height /= 2;
+        hidden_width /= 4;
+        hidden_height /= 4;
 
         // Iterate over input image and replace values with hidden pixel values
 
-        unsigned char *hidden_pixmap = new unsigned char[4 * hidden_width * hidden_height * hidden_channels]();
+        hidden_pixmap = new unsigned char[4 * hidden_width * hidden_height * hidden_channels]();
         hiddenBuffer.get_pixels(ROI::All(), TypeDesc::UINT8, hidden_pixmap);
 
-        int h_index = 0;
-        for (int i = 0; (i < img_width * img_height * channels) && (h_index < hidden_width * hidden_height * hidden_channels); i+=2){
-            // cout << pixmap[i] << endl;
-            pixmap[i] = (pixmap[i] >> 2 << 2) | (hidden_pixmap[h_index]);
-            if ((i + 1) % 4 == 0) h_index++;
-            if (i + 1 >= img_width * img_height * channels && h_index < hidden_width * hidden_height * hidden_channels) cout << "RAN OUT OF SPACE\n";
+        // int h_index = 0;
+        // int i = 0;
+        // for (i = 0; (i < img_width * img_height * channels) && (h_index < hidden_width * hidden_height * hidden_channels); i += channels-1){
+        //     // cout << pixmap[i] << endl;
+        //     pixmap[i] = (pixmap[i] & 0xFC) | (hidden_pixmap[h_index]);
+        //     if ((i + 1) % channels == 0) h_index++;
+        //     if ((i + 3 >= img_width * img_height * channels) && (h_index < hidden_width * hidden_height * hidden_channels)) cout << "RAN OUT OF SPACE\n";
+        // }
+
+        int input_pix_len = sizeof(unsigned char) * 4 * img_width * img_height * channels;
+        int hidden_pix_len = sizeof(unsigned char) * 4 * hidden_width * hidden_height * hidden_channels;
+
+        // Encode hidden length at the beginning of input image
+        for (int i = 0; i < HEADER_SIZE; i++) {
+            pixmap[i] &= 0xFC;
+            pixmap[i] |= (input_pix_len >> (HEADER_SIZE - 1 - i)) & 1UL;
         }
 
-        // hiddenBuffer.get_pixels(ROI::All(), TypeDesc::UINT8, pixmap);
-        
+        // Encode hidden image
+        for (int i = 0; i < hidden_pix_len; i++) {
+            pixmap[i+HEADER_SIZE] &= 0xFC;
+            pixmap[i+HEADER_SIZE] |= (hidden_pixmap[i/4] >> ((hidden_pix_len - 1 - i) % 4)) & 1;
+        }
 
-
-        // ImageBuf::Iterator<unsigned char> h_iter (hiddenBuffer);
-        // for (int i = 0; i < 100; i++)
-        //     cout << sizeof(hidden_pixmap[i]) << endl;
-        // int h_pixmap_index = 0;
-        // for (ImageBuf::Iterator<unsigned char> iter (readBuffer);  !iter.done();  ++iter) {
-        //     for (int c = 0;  c < channelsToRead;  ++c){
-        //         // iter[c] = ((unsigned char)iter[c] >> 2 << 2) | hidden_pixmap[h_pixmap_index];
-        //         // bitset<8> b {iter[c]};
-        //         unsigned char data = iter[c];
-        //         cout << (int)data << endl;
-        //     }
-        //     h_pixmap_index++;
-        // }
+        // cout << h_index << " : " << i << endl;
     }
 }
 
